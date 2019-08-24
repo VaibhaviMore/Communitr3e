4839ccd2fc97b10732a1b2a876db688ccddd16ad
Vaibhavi Milind More <vaibmore26@gmail.com>
2019-08-23 21:45:34 -0400

    Add files via upload

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer id="footer" class="dark" style="background: url('../images/footer-bg.jpg') repeat; background-size: cover;">
+      <div class="container">
+            <div id="copyrights">
+                  <div class="container clearfix topmargin-sm">
+                        <div class="col_two_third nomargin">
+                              <p class="text-white">Copyrights © 2019 All Rights Reserved by Communitr3e. </p>
+                        </div>
+                        <div class="col_one_third col_last tright nomargin">
+                              <div class="fright clearfix"> 
+                                    <a href="{{ url('http://www.facebook.com') }}" target="_blank" class="social-icon si-small si-borderless nobottommargin si-facebook"> <i class="icon-facebook"></i> <i class="icon-facebook"></i> </a> 
+                                    <a href="{{ url('http://www.twitter.com') }}" target="_blank"  class="social-icon si-small si-borderless nobottommargin si-twitter"> <i class="icon-twitter"></i> <i class="icon-twitter"></i> </a> 
+                                    <a href="{{ url('http://www.pinterest.com') }}" target="_blank"  class="social-icon si-small si-borderless nobottommargin si-pinterest"> <i class="icon-pinterest"></i> <i class="icon-pinterest"></i> </a> 
+                                    <a href="{{ url('http://www.viemo.com') }}" target="_blank"  class="social-icon si-small si-borderless nobottommargin si-vimeo"> <i class="icon-vimeo"></i> <i class="icon-vimeo"></i> </a> 
+                                    <a href="{{ url('http://www.github.com') }}" target="_blank"  class="social-icon si-small si-borderless nobottommargin si-github"> <i class="icon-github"></i> <i class="icon-github"></i> </a> 
+                                    <a href="{{ url('http://www.yahoo.com') }}" target="_blank"  class="social-icon si-small si-borderless nobottommargin si-yahoo"> <i class="icon-yahoo"></i> <i class="icon-yahoo"></i> </a> 
+                                    <a href="{{ url('http://www.linkedin.com') }}" target="_blank"  class="social-icon si-small si-borderless nobottommargin si-linkedin"> <i class="icon-linkedin"></i> <i class="icon-linkedin"></i> </a> 
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      </div>
+</footer> 
